944b05fc391f9ae6572c5227d2ea7654aa304499
Sumant <sumantpatil49@gmail.com>
2025-07-06 20:07:33 +0530

    merge2

--- app.py
+++ app.py
@@ -19,7 +19,10 @@ print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 
+
 choice = input("Enter your choice (1/2/3/4): ")
+
+
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
